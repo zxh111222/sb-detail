00073818a1abf0c7d00ab29d5fc12a00f08555f6
zxhhhxz1221 <1305894626@qq.com>
2024-12-03 21:31:38 +0800

    改从 Spring IoC container 获取需要的对象 ...
    Configuration Metadata + Your Business Objects = Fully configured system Ready for Use
    
    也解释了为什么叫 Inversion of Control (IoC)，原来是自己需要什么自己 new 什么，现在不自己 new 了，改从容器获取，new 的控制权交出去了

--- day241203-spring-quickstart/src/main/java/MyApplication.java
+++ day241203-spring-quickstart/src/main/java/MyApplication.java
@@ -3,15 +3,19 @@ import dao.BlogRepository;
 import dao.impl.NopeBlogRepository;
 import dao.impl.SimpleBlogRepository;
 import entity.Blog;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.BlogService;
 import service.impl.SimpleBlogService;
 
 public class MyApplication {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+
         System.out.println("MyApplication.main");
-        BlogRepository blogRepository = new NopeBlogRepository();
-        BlogService blogService = new SimpleBlogService(blogRepository);
-        BlogController blogController = new BlogController(blogService);
+//        BlogRepository blogRepository = new NopeBlogRepository();
+//        BlogService blogService = new SimpleBlogService(blogRepository);
+//        BlogController blogController = new BlogController(blogService);
+        BlogController blogController = (BlogController) applicationContext.getBean("blogController");
         Blog blog = new Blog(1L, "这是一个标题", "这是内容");
         blogController.save(blog);
     }
