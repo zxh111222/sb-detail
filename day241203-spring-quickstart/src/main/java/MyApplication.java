4b6db60e88585bb51e4f2be47130aafb632a12c5
zxhhhxz1221 <1305894626@qq.com>
2024-12-03 21:16:23 +0800

    测试+1：现在可以按需自由组装了，但是 ...
    还没有实现完全不修改 .java 代码的需求

--- day241203-spring-quickstart/src/main/java/MyApplication.java
+++ day241203-spring-quickstart/src/main/java/MyApplication.java
@@ -1,16 +1,16 @@
 import controller.BlogController;
 import dao.BlogRepository;
+import dao.impl.NopeBlogRepository;
 import dao.impl.SimpleBlogRepository;
 import entity.Blog;
 import service.BlogService;
-import service.impl.NopeBlogService;
 import service.impl.SimpleBlogService;
 
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("MyApplication.main");
-        BlogRepository blogRepository = new SimpleBlogRepository();
-        BlogService blogService = new NopeBlogService(blogRepository);
+        BlogRepository blogRepository = new NopeBlogRepository();
+        BlogService blogService = new SimpleBlogService(blogRepository);
         BlogController blogController = new BlogController(blogService);
         Blog blog = new Blog(1L, "这是一个标题", "这是内容");
         blogController.save(blog);
