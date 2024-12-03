a6bf674c0b90468d964b9e64710e8610f14f59e1
zxhhhxz1221 <1305894626@qq.com>
2024-12-03 21:08:11 +0800

    改造代码：目的是不再自己new，那得 ...
    对外开放一些窗口，方便别人传给你，
    开放方式之一：构造器

--- day241203-spring-quickstart/src/main/java/MyApplication.java
+++ day241203-spring-quickstart/src/main/java/MyApplication.java
@@ -1,10 +1,13 @@
 import controller.BlogController;
 import entity.Blog;
+import service.BlogService;
+import service.impl.SimpleBlogService;
 
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("MyApplication.main");
-        BlogController blogController = new BlogController();
+        BlogService blogService = new SimpleBlogService();
+        BlogController blogController = new BlogController(blogService);
         Blog blog = new Blog(1L, "这是一个标题", "这是内容");
         blogController.save(blog);
     }
--- day241203-spring-quickstart/src/main/java/controller/BlogController.java
+++ day241203-spring-quickstart/src/main/java/controller/BlogController.java
@@ -5,7 +5,14 @@ import service.BlogService;
 import service.impl.SimpleBlogService;
 
 public class BlogController {
-     public void save(Blog blog) {
+
+    BlogService blogService;
+
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
+
+    public void save(Blog blog) {
          // 这里只是简单举了一个小例子 - 切换 BlogController 中依赖的具体 BlogService 实现类
          // 其他地方也可能需要切换，如：BlogService 依赖的具体 BlogRepository 实现类
          // ... 真实项目要切换的地方可能很多
@@ -13,7 +20,6 @@ public class BlogController {
          // 思路提醒：现在是“自己 new” => 能否“让别人 new 好给我直接用”，现实中有很多这样的需求和例子
          // 解决方法有很多，先参考 Spring 给出的解决方案
          // 回顾之前看到的一张图：https://docs.spring.io/spring-framework/reference/core/beans/basics.html
-        BlogService blogService = new SimpleBlogService();
         blogService.save(blog);
     }
 }
