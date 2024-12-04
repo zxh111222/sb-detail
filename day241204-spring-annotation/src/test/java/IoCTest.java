7e1fc1dccdac13aa3ce782b7f96899744ed0472c
zxhhhxz1221 <1305894626@qq.com>
2024-12-04 20:48:50 +0800

    单元测试快速体验，体会它跟 sout 的区别

--- day241204-spring-annotation/src/test/java/IoCTest.java
+++ day241204-spring-annotation/src/test/java/IoCTest.java
@@ -1,4 +1,5 @@
 import com.example.config.AppConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -13,12 +14,16 @@ public class IoCTest {
     void testContainBeanBlogController() {
         boolean blogController = applicationContext.containsBean("blogController");
         System.out.println(blogController);
+
+        Assertions.assertTrue(blogController);
     }
 
     @Test
     void testNotContainBeanBlogController() {
         boolean blogController = applicationContext.containsBean("blogController_123321");
         System.out.println(blogController);
+
+        Assertions.assertTrue(blogController);
     }
 
 }
