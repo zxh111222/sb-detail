95423136585ef41f18c4866a03a018c2e7f0d023
zxhhhxz1221 <1305894626@qq.com>
2024-12-04 20:44:05 +0800

    增加 IoCTest

---
+++ day241204-spring-annotation/src/test/java/IoCTest.java
@@ -0,0 +1,24 @@
+import com.example.config.AppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(AppConfig.class)
+public class IoCTest {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void testContainBeanBlogController() {
+        boolean blogController = applicationContext.containsBean("blogController");
+        System.out.println(blogController);
+    }
+
+    @Test
+    void testNotContainBeanBlogController() {
+        boolean blogController = applicationContext.containsBean("blogController_123321");
+        System.out.println(blogController);
+    }
+
+}
