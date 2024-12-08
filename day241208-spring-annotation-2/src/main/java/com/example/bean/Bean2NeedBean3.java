282f858f14f6b48e1e910eb99288c64e8c3d5c14
zxhhhxz1221 <1305894626@qq.com>
2024-12-08 10:53:35 +0800

    DI - Setter-based dependency injection

--- day241208-spring-annotation-2/src/main/java/com/example/bean/Bean2NeedBean3.java
+++ day241208-spring-annotation-2/src/main/java/com/example/bean/Bean2NeedBean3.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Component;
 public class Bean2NeedBean3 {
     public Bean3 bean3;
 
-
-    public Bean2NeedBean3(Bean3 bean3) {
+    @Autowired
+    public void setBean3(Bean3 bean3) {
         this.bean3 = bean3;
     }
+
+//    public Bean2NeedBean3(Bean3 bean3) {
+//        this.bean3 = bean3;
+//    }
 }
