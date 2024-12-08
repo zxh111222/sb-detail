75fb75bae158d4f27fb03924a23d01a61e35847a
zxhhhxz1221 <1305894626@qq.com>
2024-12-08 10:38:34 +0800

    复习 DI - Dependency Injection 之构造器注入 - xml ...
    Dependency injection (DI)
    Constructor-based Dependency Injection
    
    https://docs.spring.io/spring-framework/reference/core/beans/dependencies/factory-collaborators.html

---
+++ day241208-spring-annotation-2/src/main/java/com/example/bean/Bean2NeedBean3.java
@@ -0,0 +1,9 @@
+package com.example.bean;
+
+public class Bean2NeedBean3 {
+    public Bean3 bean3;
+
+    public Bean2NeedBean3(Bean3 bean3) {
+        this.bean3 = bean3;
+    }
+}
--- day241208-spring-annotation-2/src/test/java/com/example/MyApplicationTest.java
+++ day241208-spring-annotation-2/src/test/java/com/example/MyApplicationTest.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import com.example.bean.Bean2NeedBean3;
+import com.example.bean.Bean3;
 import com.example.config.AppConfig;
 import com.example.controller.BlogController;
 import com.example.entity.Blog;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,6 +16,7 @@ public class MyApplicationTest {
 
 
     @Test
+    @DisplayName("XML-based")
     public void xmlTest() {
         ClassPathXmlApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
         boolean blogController = applicationContext.containsBean("blogController");
@@ -20,10 +24,21 @@ public class MyApplicationTest {
     }
 
     @Test
+    @DisplayName("Annotation-based")
     public void annotationTest() {
         AnnotationConfigApplicationContext applicationContext =  new AnnotationConfigApplicationContext(AppConfig.class);
         boolean blogController = applicationContext.containsBean("blogController");
         Assertions.assertTrue(blogController);
     }
+
+    @Test
+    @DisplayName("bean2 当中的 bean3 和容器中拿的 bean3 是同一个对象")
+    public void testBean2NeedBean3() {
+        ClassPathXmlApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
+        Bean3 bean3 = (Bean3) applicationContext.getBean("bean3");
+        Bean2NeedBean3 bean2 = applicationContext.getBean(Bean2NeedBean3.class);
+        Assertions.assertEquals(bean3, bean2.bean3);
+    }
+
 
 }
