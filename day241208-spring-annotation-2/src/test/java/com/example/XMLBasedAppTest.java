5d5f7f89bc443a4b166df0d8e47939f2040e5e4a
zxhhhxz1221 <1305894626@qq.com>
2024-12-08 11:01:24 +0800

    单元测试：@BeforeAll

--- day241208-spring-annotation-2/src/test/java/com/example/XMLBasedAppTest.java
+++ day241208-spring-annotation-2/src/test/java/com/example/XMLBasedAppTest.java
@@ -2,17 +2,26 @@ package com.example;
 
 import com.example.bean.Bean2NeedBean3;
 import com.example.bean.Bean3;
+import com.example.config.AppConfig;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class XMLBasedAppTest {
 
+    static ClassPathXmlApplicationContext applicationContext;
+
+    @BeforeAll
+    static void initApplicationContext() {
+        applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+    }
+
     @Test
     @DisplayName("XML-based")
     public void xmlTest() {
-        ClassPathXmlApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
         boolean blogController = applicationContext.containsBean("blogController");
         Assertions.assertTrue(blogController);
     }
@@ -20,7 +29,6 @@ public class XMLBasedAppTest {
     @Test
     @DisplayName("基于XML - bean2 当中的 bean3 和容器中拿的 bean3 是同一个对象")
     public void testBean2NeedBean3Xml() {
-        ClassPathXmlApplicationContext applicationContext =  new ClassPathXmlApplicationContext("beans.xml");
         Bean3 bean3 = (Bean3) applicationContext.getBean("bean3");
         Bean2NeedBean3 bean2 = applicationContext.getBean(Bean2NeedBean3.class);
         Assertions.assertEquals(bean3, bean2.bean3);
