5a3e5a589b5db9daa9d05593167e2bf87438e41f
zxhhhxz1221 <1305894626@qq.com>
2024-12-08 15:13:40 +0800

    模拟 AOP - 统计方法执行时间 - 最直观的写法

--- day241208-summer/src/main/java/com/example/proxy/Girl.java
+++ day241208-summer/src/main/java/com/example/proxy/Girl.java
@@ -9,11 +9,17 @@ public class Girl implements Flyable {
 
     @Override
     public void fly() {
+        long start = System.currentTimeMillis();
+        System.out.println("start = " + start);
+
         System.out.println("Believe me I can fly, I am singing in the sky...");
         try {
             Thread.sleep(new Random().nextInt(2 * 1000));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        long end = System.currentTimeMillis();
+        System.out.println("fly time = " + (end - start));
     }
 }
