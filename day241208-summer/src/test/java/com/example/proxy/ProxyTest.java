7efde809064632d1fdf2a3af09c4616f320b4289
zxhhhxz1221 <1305894626@qq.com>
2024-12-08 15:35:38 +0800

    模拟 AOP - 思考 - 见代码注释

--- day241208-summer/src/test/java/com/example/proxy/ProxyTest.java
+++ day241208-summer/src/test/java/com/example/proxy/ProxyTest.java
@@ -1,5 +1,6 @@
 package com.example.proxy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProxyTest {
@@ -24,6 +25,13 @@ public class ProxyTest {
     public void GirlLoggingProxy() {
         Girl target = new Girl();
         new GirlLoggingProxy(target).fly();
+    }
+
+    @Test
+    @DisplayName("即统计时间，也记录日志（先时间、再日志）")
+    void testGirlTimerAndLogging() {
+        // todo 思考：如何实现“即记录日志，也统计时间”的功能
+        // 同时思考：如何灵活的调整“记录日志”和“统计时间”的顺序
     }
 
 }
