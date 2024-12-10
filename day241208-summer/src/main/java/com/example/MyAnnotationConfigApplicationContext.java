9d6d20d480ae41374135dea077908a13e9058213
zxhhhxz1221 <1305894626@qq.com>
2024-12-10 21:12:15 +0800

    模拟基于注解的 Spring IoC 容器 - 解析特定注解之 @MyConfiguration

--- day241208-summer/src/main/java/com/example/MyAnnotationConfigApplicationContext.java
+++ day241208-summer/src/main/java/com/example/MyAnnotationConfigApplicationContext.java
@@ -1,14 +1,47 @@
 package com.example;
 
+import com.example.annotation.MyComponentScan;
+import com.example.annotation.MyConfiguration;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyAnnotationConfigApplicationContext implements MyBeanFactory {
 
+    Map<String, Object> beansMap = new HashMap<String, Object>();
+
     public MyAnnotationConfigApplicationContext(Class<?> configClass) {
-        System.out.println(configClass.getName());
+        Annotation[] annotations = configClass.getAnnotations();
+        System.out.println(Arrays.toString(annotations));
+
+        try{
+            for (Annotation annotation : annotations) {
+                if (annotation instanceof MyConfiguration) {
+                    Object o = configClass.getDeclaredConstructor().newInstance();
+                    beansMap.put(getKeyName(configClass), o);
+                } else if(annotation instanceof MyComponentScan) {
+
+                }
+            }
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private String getKeyName(Class<?> cls) {
+        String className = cls.getName().replace(cls.getPackageName() + ".", "");
+        //System.out.println(className);
+        String key = className.substring(0, 1).toLowerCase() + className.substring(1);
+        //System.out.println(key);
+        return key;
     }
 
     @Override
     public Object getBean(String name) {
-        return null;
+        return beansMap.get(name);
     }
 
     @Override
--- day241208-summer/src/test/java/com/example/AnnotationBasedMyIoCTest.java
+++ day241208-summer/src/test/java/com/example/AnnotationBasedMyIoCTest.java
@@ -2,12 +2,21 @@ package com.example;
 
 
 import com.example.config.AppConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AnnotationBasedMyIoCTest {
     @Test
     public void xxx() {
         MyAnnotationConfigApplicationContext myAnnotationConfigApplicationContext = new MyAnnotationConfigApplicationContext(AppConfig.class);
+    }
+
+    @Test
+    public void xxxx() {
+        MyAnnotationConfigApplicationContext myAnnotationConfigApplicationContext = new MyAnnotationConfigApplicationContext(AppConfig.class);
+        Object appConfig = MyAnnotationConfigApplicationContext.getBean("appConfig");
+        Assertions.assertNotNull(appConfig);
+
     }
 
 }
