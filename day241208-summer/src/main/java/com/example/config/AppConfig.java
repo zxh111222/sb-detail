3fd6b579aa6253f1c2bc01d9557e345528e616f3
zxhhhxz1221 <1305894626@qq.com>
2024-12-10 21:12:28 +0800

    模拟基于注解的 Spring IoC 容器 - 增加并使用 @MyComponentScan 自定义注解

--- day241208-summer/src/main/java/com/example/config/AppConfig.java
+++ day241208-summer/src/main/java/com/example/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.example.config;
 
 
+import com.example.annotation.MyComponentScan;
 import com.example.annotation.MyConfiguration;
 
 @MyConfiguration
+@MyComponentScan("com.example")
 public class AppConfig {
 }
